06592d11add6463d6eaae1140b286771e4ccdd1c
Natalia Kostka <natalia7kostka@gmail.com>
2022-11-04 13:34:39 -0600

    finished crash course

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars')
+const logger = require('./Middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-});
+//initializing middleware
+// app.use(logger);
+
+//handlebars middleware
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//homepage render
+app.get('/', (req, res) => res.render('index', {
+    title: "Member App",
+    members 
+}));
+
+//setting a static folder 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//member API routes
+app.use('/api/members', require('./Routes/api/members'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
